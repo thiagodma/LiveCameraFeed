f45c32733e757a3303fa4397f48360d600ab0431
Thiago Dantas <thiagodma52@gmail.com>
2020-04-02 20:49:39 -0300

    finished the save_stats method

--- src/true_value.py
+++ src/true_value.py
@@ -1,7 +1,7 @@
 import numpy as np
 import tensorflow as tf
 import pandas as pd
-import cv2, time, socket, struct, warnings, os
+import cv2, time, socket, struct, warnings, os, csv
 import face_recognition
 from datetime import datetime
 from pyagender import PyAgender
@@ -18,7 +18,27 @@ class TrueValue():
         self.graphs_path = graphs_path
 
     def save_stats(self,num_persons,ages,num_males,num_females,now):
+        if not os.path.isfile('../data.csv'):
+            df = pd.DataFrame(columns=['time','num_male','num_female',
+                                       'num_persons','(0-10)','(10-20)',
+                                       '(20-30)','(30-40)','(40-50)',
+                                       '(50-60)','(60-70)','(70-inf)'])
+            df.to_csv('../data.csv',index=False)
 
+        row = [now,num_males,num_females,num_persons] + [0]*8
+        for age in ages:
+            if age <= 10: row[4]+=1
+            elif age > 10 and age <=20: row[5]+=1
+            elif age > 20 and age <=30: row[6]+=1
+            elif age > 30 and age <=40: row[7]+=1
+            elif age > 40 and age <=50: row[8]+=1
+            elif age > 50 and age <=60: row[9]+=1
+            elif age > 60 and age <=70: row[10]+=1
+            elif age > 70: row[11]+=1
+
+        with open('../data.csv','a') as f:
+            writer = csv.writer(f)
+            writer.writerow(row)
 
 
     def run(self):
