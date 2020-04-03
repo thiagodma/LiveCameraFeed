b01fa9adac4ce429239388a7846c61cc3e044a49
Thiago Dantas <thiagodma52@gmail.com>
2020-04-03 18:38:52 -0300

    fixed spelling mistakes

--- src/true_value.py
+++ src/true_value.py
@@ -13,15 +13,15 @@ class TrueValue():
         self.human_detector = human_detector        #class that does the human detection
         self.agender_detector = agender_detector    #class that does age and gender detection
 
-    def save_stats(self,num_persons,ages,num_males,num_females,now):
+    def save_stats(self,num_people,ages,num_males,num_females,now):
         if not os.path.isfile('data.csv'):
             df = pd.DataFrame(columns=['time','num_male','num_female',
-                                       'num_persons','(0-10)','(10-20)',
+                                       'num_people','(0-10)','(10-20)',
                                        '(20-30)','(30-40)','(40-50)',
                                        '(50-60)','(60-70)','(70-inf)'])
             df.to_csv('data.csv',index=False)
 
-        row = [now,num_males,num_females,num_persons] + [0]*8
+        row = [now,num_males,num_females,num_people] + [0]*8
         for age in ages:
             if age <= 10: row[4]+=1
             elif age > 10 and age <=20: row[5]+=1
@@ -49,7 +49,7 @@ class TrueValue():
         faces = self.agender_detector.detect_genders_ages(img)
 
         #calculates the number of persons in the image and puts the bounding boxes
-        num_persons, img = self.human_detector.process_frame(boxes, scores, classes, img)
+        num_people, img = self.human_detector.process_frame(boxes, scores, classes, img)
 
         #puts the bounding boxes and returns lists with ages and genders
         ages, num_males, num_females, img = self.agender_detector.process_frame(faces,img)
@@ -58,7 +58,7 @@ class TrueValue():
         #cv2.imwrite(self.proc_img_path + '/' + now + '.jpg',img)
 
         #saves the stats
-        self.save_stats(num_persons,ages,num_males,num_females,now)
+        self.save_stats(num_people,ages,num_males,num_females,now)
 
         return img
 
@@ -67,14 +67,17 @@ if __name__ == '__main__':
     #getting the live feed
     #cap = cv2.VideoCapture()
     #cap.open("http://81.14.37.24:8080/mjpg/video.mjpg?timestamp=1585844515370")
-    model_path = 'faster_rcnn_inception_v2_coco_2018_01_28/frozen_inference_graph.pb'
-    human_detector = HumanDetector(model_path)
+    #model_path = 'faster_rcnn_inception_v2_coco_2018_01_28/frozen_inference_graph.pb'
+    model_path = 'ssd_mobilenet_v1_coco_2017_11_17/frozen_inference_graph.pb'
+    human_detector = HumanDetector(model_path,threshold=0.2)
     agender_detector = FaceAgeGenderDetection()
     truevalue = TrueValue(human_detector,agender_detector)
 
     cap = cv2.VideoCapture('data/classroom.mp4')
+    # cap = cv2.VideoCapture('data/face-demographics-walking.mp4')
+    fr=30
     while True:
-        for _ in range(30):
+        for _ in range(fr):
             r, img = cap.read()
         img = cv2.resize(img, (640, 400))
         img = truevalue.run(img)
